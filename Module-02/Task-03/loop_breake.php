3290f5694042e163837c6411a29524e30581152c
Hanif-ai <1996hanifmial@gmai.com>
2023-09-21 23:18:10 +0600

    update

--- Module-02/Task-03/loop_breake.php
+++ Module-02/Task-03/loop_breake.php
@@ -3,15 +3,14 @@
     $sum1 = 0;
     $sum2 = 1;
 
-    for($i  = 0; $i <= 20; $i++){
-
+    for($i  = 0; $i <= 10; $i++){
+        if($sum1 > 100){
+            break;
+        };
         echo $sum1."\n";
         $result = $sum1 + $sum2;
         $sum1 = $sum2; 
         $sum2 = $result; 
-        if($result > 100){
-            break;
-        };
     }
 
 ?>

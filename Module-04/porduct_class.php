d375fe3a249a430a0c642b876fb2d8009b916717
Md Hanif Mia <67138388+Hanif-ai@users.noreply.github.com>
2023-10-07 23:11:06 +0600

    Update porduct_class.php

--- Module-04/porduct_class.php
+++ Module-04/porduct_class.php
@@ -1,3 +1,24 @@
 <?php
+class Product{
+    protected $id;
+    protected $name;
+    protected $price;
+    public function __construct(int $id,string $name,float $price){
+        $this->name = $name;
+        $this->id = $id;
+        $this->price = $price;
+    }
+    public function formatePrice(){
+        return "Price: $".number_format($this->price,2);
+    }
+    public function showDetails(){
+        echo "ID: {$this->id}\n";
+        echo "Name: {$this->name}\n";
+        echo "{$this->formatePrice()}";
+    }
+}
+
+$product  = new Product(1,"T-shirt",19.99);
+$product->showDetails();
 
 ?>
